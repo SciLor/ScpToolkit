b0ffdfd49b6ed376eee22ddda41a40ed1ab0ffb9
Radik Kurbanov <rkurbanov@therecongroup.com>
2017-09-29 18:17:41 +0300

    Delete scp monitor. This functionality will be inside ScpProfiler

--- ScpMonitor/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using log4net.Config;
-
-[assembly: AssemblyTitle("ScpMonitor")]
-[assembly: AssemblyProduct("ScpMonitor")]
-
-[assembly: Guid("85966027-b1d8-4a14-bc29-36f9091db86b")]
-[assembly: XmlConfigurator(Watch = true)]
